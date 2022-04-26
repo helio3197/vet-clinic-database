c98df140067813abdbbea52e39636bf1fee27930
Kenny <kennysalazar3197@gmail.com>
2022-04-26 13:41:57 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-id int,
+id int GENERATED ALWAYS AS IDENTITY,
 name varchar(100),
 date_of_birth date,
 escape_attempts int,
 neutered bit,
-weight_kg decimal
+weight_kg decimal,
+PRIMARY KEY (id)
 );
